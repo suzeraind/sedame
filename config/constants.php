72540ca14a942e670b29a4cd4f68de9ed63f8d8c
suzeraind <nikitas4478@gmail.com>
2025-07-22 14:58:03 +0300

    add constant to the project

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+// Определяем корневую директорию проекта
+define('BASE_PATH', dirname(__DIR__));
+
+// Пути к важным папкам
+define('VIEW_PATH', BASE_PATH . '/app/Views');
+define('PUBLIC_PATH', BASE_PATH . '/public');
+define('CORE_PATH', BASE_PATH . '/app/Core');
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/constants.php';
 
 use App\Core\Init;
 
